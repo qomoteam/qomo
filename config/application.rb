6e2b2258259cd84d124d3995368ae5947a4e2ee3
xingjian <xuxingjian@big.ac.cn>
2015-11-11 22:31:38 +0800

    QOMO-84 Email verification

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Qomo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_mailer.smtp_settings = config_for(:email)['smtp_settings'].symbolize_keys
+    config.action_mailer.default_url_options = config_for(:email)['default_url_options'].symbolize_keys
   end
 end
