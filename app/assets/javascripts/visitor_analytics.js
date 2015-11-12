3e566486af36fb86828f268438740a6befad9c5f
xingjian <xuxingjian@big.ac.cn>
2015-11-12 12:35:18 +0800

    QOMO-113 Add user trace analysis script

---
+++ app/assets/javascripts/visitor_analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?e278349fdb734e8a31183ce0ddbec963";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
