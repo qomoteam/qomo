4f6e6ee17d9cb3fa306d9007df46f67cdab11bc5
xingjian <xuxingjian@big.ac.cn>
2015-12-25 18:34:41 +0800

    QOMO-83 Sign up with captcha protect

---
+++ config/initializers/rucaptcha.rb
@@ -0,0 +1,5 @@
+RuCaptcha.configure do
+  self.len = 4
+  self.font_size = 45
+  self.cache_limit = 100
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
+
+  mount RuCaptcha::Engine => '/captcha'
 
   root 'workspaces#show'
 
