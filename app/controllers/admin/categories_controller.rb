a112a986bd02477688311ab193e95aa920e884d1
xingjian <xuxingjian@big.ac.cn>
2016-04-25 17:08:53 +0800

    QOMO-157 CRUD for category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,4 +13,23 @@ class Admin::CategoriesController < Admin::ApplicationController
     redirect_to action: 'index'
   end
 
+
+  def edit
+    @category = Category.find params[:id]
+  end
+
+
+  def update
+    category = Category.find params['id']
+    category.update params.require('category').permit!
+    redirect_to admin_categories_path
+  end
+
+
+  def destroy
+    category = Category.find params['id']
+    category.delete!
+    redirect_to admin_categories_path
+  end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,10 @@
 class Category < ActiveRecord::Base
+
+  UNCATEGORY_ID = -1
+
   acts_as_nested_set
+
+  default_scope -> { order :name }
 
   has_many :tools
 
@@ -7,6 +12,18 @@ class Category < ActiveRecord::Base
 
   def active_tools
     self.tools.select { |tool| tool.active? }
+  end
+
+
+  def delete!
+    Pipeline.where(category_id: self.id).update_all(category_id: Category::UNCATEGORY_ID)
+    Tool.where(category_id: self.id).update_all(category_id: Category::UNCATEGORY_ID)
+    self.delete
+  end
+
+
+  def self.uncategory
+    Category.find Category::UNCATEGORY_ID
   end
 
 end
