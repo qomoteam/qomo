8bb1a0b481073d3e12d077ab509926d084e734ed
xingjian <xuxingjian@big.ac.cn>
2016-09-05 12:49:47 +0800

    qomo-19 No elements in minimap

--- app/service/rmq.rb
+++ app/service/rmq.rb
@@ -15,4 +15,14 @@ class RMQ
     end
   end
 
+  def subscribe!
+    q = channel.queue("qomo_#{Rails.env}.jobs.status", durable: true, exclusive: false, auto_delete: false)
+    q.subscribe do |delivery_info, metadata, payload|
+      juid = payload
+      job_unit = JobUnit.find(juid)
+      uid = job_unit.job.user.id
+      ActionCable.server.broadcast("job_manager_#{uid}", job_id: job_unit.job.id, job_unit_id: job_unit.id)
+    end
+  end
+
 end
