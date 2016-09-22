6a89c89bb00c034b2321c0ab13fb839668e8fbb2
xingjian <xuxingjian@big.ac.cn>
2016-09-22 22:07:03 +0800

    qomo-85 Sort by file name in file selector

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -17,7 +17,8 @@ class LibraryController < ApplicationController
             id: "@#{owner.username}:#{e.path}",
             children: e.directory?,
             #TODO remove frontend html class
-            icon: e.directory? ? 'fa fa-folder' : 'fa fa-file-o'
+            icon: e.directory? ? 'fa fa-folder' : 'fa fa-file-o',
+            type: e.directory? ? 'dir' : 'file'
         }
       end
     else
@@ -28,13 +29,14 @@ class LibraryController < ApplicationController
             id: "@#{record.owner.username}:#{record.path}",
             children: record.meta.directory?,
             #TODO remove frontend html class
-            icon: record.meta.directory? ? 'fa fa-folder' : 'fa fa-file-o'
+            icon: record.meta.directory? ? 'fa fa-folder' : 'fa fa-file-o',
+            type: record.meta.directory? ? 'dir' : 'file'
         }
       end
     end
 
 
-    render json: tree
+    render json: tree.sort_by { |e| [e[:type], e[:text].downcase]}
   end
 
 end
