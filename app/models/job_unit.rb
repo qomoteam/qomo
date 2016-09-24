5175cc835b3462a5f3cfba61ea72b023474ca37b
xingjian <xuxingjian@big.ac.cn>
2016-09-24 20:50:13 +0800

    qomo-102 Drop docker container when delete job

--- app/models/job_unit.rb
+++ app/models/job_unit.rb
@@ -2,15 +2,27 @@ class JobUnit < ApplicationRecord
 
   include StatusAware
 
+  before_destroy :drop_docker_container
+
   belongs_to :job
   belongs_to :tool
 
   def out
     if self.docker
-      Docker::Container.get(self.docker['cid'], {}, Docker::Connection.new(self.docker['host'], {})).logs(stdout: true, stderr: true)
+      docker_container.logs(stdout: true, stderr: true)
     else
       ''
     end
+  end
+
+  def drop_docker_container
+    if self.docker
+      docker_container.remove(v: true, force: true)
+    end
+  end
+
+  def docker_container
+    Docker::Container.get(self.docker['cid'], {}, Docker::Connection.new(self.docker['host'], {}))
   end
 
 end
