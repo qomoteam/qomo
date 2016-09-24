98a5de4ad7b7584d1a6d59f71dcad5df55873068
xingjian <xuxingjian@big.ac.cn>
2016-09-24 20:09:34 +0800

    qomo-101 Get job unit log from webapp, not qomo-engine

--- app/models/job_unit.rb
+++ app/models/job_unit.rb
@@ -5,5 +5,8 @@ class JobUnit < ApplicationRecord
   belongs_to :job
   belongs_to :tool
 
+  def out
+    Docker::Container.get(self.docker.cid, {}, Docker::Connection.new(self.docker.host, {})).logs
+  end
 
 end
