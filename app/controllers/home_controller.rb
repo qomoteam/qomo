182fed396f6683cc8805b21cd79ccfd08bec65bc
xingjian <xuxingjian@big.ac.cn>
2016-09-26 22:06:39 +0800

    qomo-136 Display total number of tools and pipelines in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
+
   def index
     @pipelines = Pipeline.where(shared: true).order(:cached_votes_score => :desc).limit(5)
     @tools = Tool.active.limit(16)
+    @count[:tools] = Tool.active.count
+    @count[:pipelines] = Pipeline.where(shared: true).count
   end
 
   def about
