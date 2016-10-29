464ef7739be515b9bd1327139ef376c56611e349
xingjian <xuxingjian@big.ac.cn>
2016-10-29 17:59:24 +0800

    qomo-221 Permalink for tools and pipelines

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -4,8 +4,17 @@ module LinksHelper
     link_to tool.name, user_tool_path(tool.owner.username, tool.slug)
   end
 
+  def tool_permalink(tool)
+    link_to tool_url(tool.accession_label), tool_url(tool.accession_label)
+  end
+
+
   def pipeline_link(pipeline)
     link_to pipeline.title, user_pipeline_path(pipeline.owner.username, pipeline.slug)
+  end
+
+  def pipeline_permalink(pipeline)
+    link_to pipeline_url(pipeline.accession_label), pipeline_url(pipeline.accession_label)
   end
 
   def category_tools_link(category, show_count=false)
