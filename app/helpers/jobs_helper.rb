32b60aa8bbe7532c93b8dfa9aefebe4f14e4b117
xingjian <xuxingjian@big.ac.cn>
2016-11-16 17:10:08 +0800

    qomo-267 Job running time calculation error

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,7 +1,7 @@
 module JobsHelper
 
   def running_time(job)
-    distance_of_time_in_words(job.created_at, job.ended_at || Time.now)
+    distance_of_time_in_words(job.started_at, job.ended_at || Time.now)
   end
 
 end
